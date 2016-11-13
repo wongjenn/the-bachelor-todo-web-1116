76b4728fa0357057fc28643e886e180f0b70c8cc
Jennifer Wong <wongjenn@yahoo.com>
2016-11-13 03:07:59 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,20 +1,15 @@
 def get_first_name_of_season_winner(data, season)
-  data.each do |seasons, contestants|
-    if seasons == season
-      contestants.each do |contestant|
-        if contestant["status"] == "Winner"
-          name = contestant["name"].split(" ")
-          return name[0]
-        end
-      end
+  data[season].each do |contestant|
+    if contestant["status"] == "Winner"
+      name = contestant["name"].split(" ")
+      return name[0]
     end
   end
-
 end
 
 def get_contestant_name(data, occupation)
   data.each do |seasons, contestants|
-    contestants.each do |contestant, values|
+    contestants.each do |contestant|
       if contestant["occupation"] == occupation
         return contestant["name"]
       end
@@ -25,7 +20,7 @@ end
 def count_contestants_by_hometown(data, hometown)
   count = 0
   data.each do |seasons, contestants|
-    contestants.select { |contestant, values| contestant["hometown"] == hometown ? count += 1 : count }
+    contestants.select { |contestant| contestant["hometown"] == hometown ? count += 1 : count }
   end
 
   return count
@@ -33,7 +28,7 @@ end
 
 def get_occupation(data, hometown)
   data.each do |seasons, contestants|
-    contestants.each do |contestant, values|
+    contestants.each do |contestant|
       if contestant["hometown"] == hometown
         return contestant["occupation"]
       end
@@ -48,7 +43,6 @@ def get_average_age_for_season(data, season)
     total_age += contestant["age"].to_f
     count += 1
   end
-  puts total_age, count
 
   (total_age/count).round(0)
 end
